86dd93d5c6ad3938f4531066cc622bebc27147f0
jluis-lira <js.black.green@gmail.com>
2021-03-19 00:09:07 -0600

    POST, Body Parser &  Postman

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const {pokemon} = require('./pokedex.json');
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 /*
 GET obtener recursos
 POST almacenar o crear un recurso
@@ -12,15 +15,20 @@ app.get("/",(req,res,next)=>{
 	return res.status(200).send("Bienvenido al Pokedex");
 });
 
-app.get('/pokemon/all',(req,res,next)=>{
+app.post('/pokemon',(req,res,next)=>{
+	return res.status(200).send(req.body);
+});
+
+app.get('/pokemon',(req,res,next)=>{
 	return res.status(200).send(pokemon);
 });
 
 app.get('/pokemon/:id([0-9]{1,3})',(req,res,next)=>{
 	const id = req.params.id - 1;
-	(id >= 0 && id <= 150) ?
-		res.status(200).send(pokemon[req.params.id - 1]):
-		res.status(404).send("Pokémon no encontrado");
+	if (id >= 0 && id <= 150) {
+		return res.status (200).send(pokemon [req.params.id - 1]);
+	}
+	return res.status(404).send("Pokémon no encontrado");
 });
 
 app.get('/pokemon/:name([A-Za-z]+)',(req,res,next)=>{
