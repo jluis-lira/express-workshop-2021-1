727fd95dbbb5fedad952abcd4dd2367f4ca764a2
jluis-lira <js.black.green@gmail.com>
2021-09-01 23:12:53 -0500

    Middleware para 404 y cambiando el body-parser

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
 const pokemon = require('./routes/pokemon');
 
 app.use(morgan('dev'));
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//app.use(bodyParser.json()); 
+//app.use(bodyParser.urlencoded({ extended: true }));
 /*
 GET obtener recursos
 POST almacenar o crear un recurso
@@ -15,10 +18,15 @@ PUT modificar un recurso
 DELETE borrar un recurso
 */
 app.get("/",(req,res,next)=>{
-	return res.status(200).send("Bienvenido al Pokedex");
+	return res.status(200).json({code:1, message:"Bienvenido al Pokedex"});
 });
 
 app.use("/pokemon",pokemon);
+
+app.use((req,res,next)=>{
+	return res.status(404).json({code: 404,message:"URL no encontrada"});
+});
+
 
 app.listen(process.env.PORT || 3000,()=>{
 	console.log("Server is running...");
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,29 +4,29 @@ const pokemon = express.Router();
 const db = require('../config/database');
 
 pokemon.post('/',(req,res,next)=>{
-	return res.status(200).send(req.body);
+	return res.status(200).json(req.body);
 });
 
 pokemon.get('/',async (req,res,next)=>{
 	const pkmn = await db.query("SELECT * FROM pokemon");
-	return res.status(200).json(pkmn);
+	return res.status(200).json({code:1,message:pkmn});
 });
 
 pokemon.get('/:id([0-9]{1,3})', async (req,res,next)=>{
 	const id = req.params.id - 1;
-	if (id >= 0 && id <= 150) {
-		const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = "+[id+1]+"");
-		return res.status(200).json(pkmn);
+	if (id >= 1 && id <= 722) {
+		const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = "+[id+1]+";");
+		return res.status(200).json({code:1,message:pkmn});
 	}
-	return res.status(404).send("Pokémon no encontrado");
+	return res.status(404).json({code: 404,message:"Pokémon no encontrado"});
 });
 
 pokemon.get('/:name([A-Za-z]+)', async (req,res,next)=>{
 	const name = req.params.name;
 	const x = 'bulbasaur';
-	const pkmn = await db.query("SELECT * FROM pokemon WHERE UPPER(pok_name) = UPPER('"+name+"')"); 
+	const pkmn = await db.query("SELECT * FROM pokemon WHERE UPPER(pok_name) = UPPER('"+name+"');"); 
 	(pkmn.length > 0) ?
-		res.status(200).json(pkmn) : 
-		res.status(404).send("Pokémon no encontrado");
+		res.status(200).json({code:1,message:pkmn}) : 
+		res.status(404).json({code: 404,message:"Pokémon no encontrado"});
 });
 module.exports = pokemon;
