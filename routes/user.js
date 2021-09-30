245a7248aee07dc1f5a1fb1227c81a8f09d21621
jluis-lira <js.black.green@gmail.com>
2021-09-30 00:14:14 -0500

    Conectando con la interfaz de usuario (2)

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,12 @@ const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
 
-user.post('/', async (req,res,next)=>{
-	const{user_name, user_mail, user_password}=req.body;
-
-	if(user_name && user_mail && user_password ){	
-		let query = "INSERT INTO user(user_name,user_mail,user_password) ";
+user.post('/signin', async (req,res,next)=>{
+	const{user_name,user_mail, user_password}=req.body;
+	if(user_name && user_mail && user_password){	
+		let query = "INSERT INTO user (user_name, user_mail, user_password)";
 		query+=` VALUES ('${user_name}','${user_mail}','${user_password}')`;
+
 		const rows= await db.query(query);
 
 		if(rows.affectedRows==1){
@@ -17,8 +17,8 @@ user.post('/', async (req,res,next)=>{
 		return res.status(500).json({code: 500,message:"Ocurrió un error."});
 	}
 	return res.status(500).json({code: 500,message:"Campos incompletos."});
-});
 
+});
 user.post('/login', async (req,res,next)=>{
 	const{user_mail, user_password}=req.body;
 	const query =`SELECT * FROM user WHERE user_mail='${user_mail}' AND user_password='${user_password}'`;
@@ -35,7 +35,7 @@ user.post('/login', async (req,res,next)=>{
 			return res.status(200).json({code: 200,message:"Usuario y/o contraseña incorrectas"});
 		}
 	}
-	return res.status(500).json({code: 500,message:"Campos incompletos."});
+	return res.status(401).json({code: 401,message:"Campos incompletos."});
 });
 user.get('/', async (req,res,next)=>{
 	const query = ("SELECT * FROM user");
