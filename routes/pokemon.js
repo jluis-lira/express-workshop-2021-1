131f355ef3d7adfc512bda8d9a3155a449143bbf
jluis-lira <js.black.green@gmail.com>
2021-10-04 23:47:57 -0500

    Conectando con la interfaz de usuario (3) y Corrección de ERRORES

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -54,13 +54,13 @@ pokemon.patch('/:id([0-9]{1,3})', async (req,res,next)=>{
 });
 pokemon.get('/', async (req,res,next)=>{
 	const pkmn = await db.query("SELECT * FROM pokemon");
-	return res.status(200).json({code:1,message:pkmn});
+	return res.status(200).json({code:200,message:pkmn});
 });
 pokemon.get('/:id([0-9]{1,3})', async (req,res,next)=>{
 	const id = req.params.id - 1;
 	if (id >= 1 && id <= 722) {
 		const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = "+[id+1]+";");
-		return res.status(200).json({code:1,message:pkmn});
+		return res.status(200).json({code:200,message:pkmn});
 	}
 	return res.status(404).json({code: 404,message:"Pokémon no encontrado"});
 });
@@ -69,7 +69,7 @@ pokemon.get('/:name([A-Za-z]+)', async (req,res,next)=>{
 	const x = 'bulbasaur';
 	const pkmn = await db.query("SELECT * FROM pokemon WHERE UPPER(pok_name) = UPPER('"+name+"');"); 
 	(pkmn.length > 0) ?
-		res.status(200).json({code:1,message:pkmn}) : 
+		res.status(200).json({code:200,message:pkmn}) : 
 		res.status(404).json({code: 404,message:"Pokémon no encontrado"});
 });
 module.exports = pokemon;
